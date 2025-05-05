bff1f74890056051785e63d8d6522345310ff352
Kurt-Leopard <markangeloujamandre@gmail.com>
2025-05-05 15:48:41 +0800

    merge

--- components/blocks/Slider.jsx
+++ components/blocks/Slider.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import "slick-carousel/slick/slick.css";
+import Slick from "react-slick";
 import globalState from "@/lib/store/globalState";
 import dynamic from "next/dynamic";
 import { useEffect, useState, useRef } from "react";
